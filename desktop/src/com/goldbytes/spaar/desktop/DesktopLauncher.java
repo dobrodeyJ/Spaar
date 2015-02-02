78c959a330b7d5d8e0bc441f43b9d39ddd505e1e
dobrodeyJ <dobrode@bigmir.net>
2015-02-02 20:45:24 +0200

    init

---
+++ desktop/src/com/goldbytes/spaar/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.goldbytes.spaar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.goldbytes.spaar.Spaar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 480;
+		config.height = 720;
+		new LwjglApplication(Spaar.getInstance(), config);
+		
+	}
+}
