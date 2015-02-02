392868bcf9e7dc6d68767e4eb9dc07b8954054b1
dobrodeyJ <dobrode@bigmir.net>
2015-02-02 20:42:39 +0200

    init
    содержит различные константы которые будем использовать в проекте

---
+++ core/src/com/goldbytes/utils/Constants.java
@@ -0,0 +1,12 @@
+package com.goldbytes.utils;
+
+import com.badlogic.gdx.Gdx;
+
+public class Constants {
+	public final static float DISPLAY_WIDTH = Gdx.graphics.getWidth();
+	public final static float DISPLAY_HEIGHT = Gdx.graphics.getHeight();
+	
+	public final static float GAME_WIDTH =  360;
+	public final static float GAME_HEIGHT = 480; 
+	
+}
