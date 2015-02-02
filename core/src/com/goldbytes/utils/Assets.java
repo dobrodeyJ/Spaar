9ab36a74521d65d623a81c6dd90c5b449785a409
dobrodeyJ <dobrode@bigmir.net>
2015-02-02 20:44:08 +0200

    init
    Подгружает Ресурсы самого проекта

---
+++ core/src/com/goldbytes/utils/Assets.java
@@ -0,0 +1,8 @@
+package com.goldbytes.utils;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+
+public class Assets {
+	public static Skin skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
+}
